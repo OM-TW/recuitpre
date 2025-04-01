59fa1dab6ada8e40fee58545df7dc0289ee41342
jameshsu1125 <jameshsu1125@gmail.com>
2025-04-01 15:24:04 +0800

    調整 Dialog 和 Tablist 組件的樣式，優化佈局和結構

--- src/pages/home/position/tablist.tsx
+++ src/pages/home/position/tablist.tsx
@@ -7,13 +7,16 @@ const Tablist = memo(() => {
   const [state, setState] = useContext(PositionContext);
 
   return (
-    <div className='w-full flex flex-row mt-12 space-x-5'>
-      <Button className='w-1/2' onClick={() => setState((S) => ({ ...S, index: 0 }))}>
+    <div className='mt-12 flex w-full flex-row space-x-5'>
+      <Button className='w-full' onClick={() => setState((S) => ({ ...S, index: 0 }))}>
+        <Button.Position job='廣告業務' eng='Account Executive' active={state.index === 0} />
+      </Button>
+      {/* <Button className='w-1/2' onClick={() => setState((S) => ({ ...S, index: 0 }))}>
         <Button.Position job='廣告業務' eng='Account Executive' active={state.index === 0} />
       </Button>
       <Button className='w-1/2' onClick={() => setState((S) => ({ ...S, index: 1 }))}>
         <Button.Position job='公關專員' eng='Account Executive' active={state.index === 1} />
-      </Button>
+      </Button> */}
     </div>
   );
 });
